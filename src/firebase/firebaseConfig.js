7cf0914ea324c1e4c501273e7ba43013dcbf7e10
Iresharma <iresh.sharma8@gmail.com>
2021-04-20 03:07:02 +0530

    firebase

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+export default { db, auth };
